b8749fe88f199503a8cf66062354665c377211bf
Waseem Ahmad Mughal <waseem.ahmad.mughal@gmail.com>
2025-06-05 20:07:22 +0500

    Remove workflow management GUI

--- serene/src/Serene.Web/Modules/ServerTypes/Workflow.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Workflow/Client/WorkflowService";
-export * from "./Workflow/WorkflowDefinitionColumns";
-export * from "./Workflow/WorkflowDefinitionForm";
-export * from "./Workflow/WorkflowDefinitionRow";
-export * from "./Workflow/WorkflowDefinitionService";
