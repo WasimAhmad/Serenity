d6c0bd8f377fc70e99f5bffebaa710b3d31cc6ac
Waseem Ahmad Mughal <waseem.ahmad.mughal@gmail.com>
2025-06-05 15:43:56 +0500

    fix workflow service esm

--- serene/src/Serene.Web/Modules/Workflow/Client/WorkflowService.ts
+++ serene/src/Serene.Web/Modules/Workflow/Client/WorkflowService.ts
@@ -1,29 +1,29 @@
-namespace Serene.Workflow {
-    export interface ExecuteWorkflowActionRequest {
-        WorkflowKey: string;
-        CurrentState: string;
-        Trigger: string;
-        Input?: any;
+import { serviceRequest } from "@serenity-is/corelib";
+
+export interface ExecuteWorkflowActionRequest {
+    WorkflowKey: string;
+    CurrentState: string;
+    Trigger: string;
+    Input?: any;
+}
+
+export interface GetPermittedActionsRequest {
+    WorkflowKey: string;
+    CurrentState: string;
+}
+
+export interface GetPermittedActionsResponse {
+    Actions: string[];
+}
+
+export namespace WorkflowService {
+    export const baseUrl = 'Services/Workflow';
+
+    export function ExecuteAction(request: ExecuteWorkflowActionRequest) {
+        return serviceRequest(baseUrl + '/ExecuteAction', request);
     }
 
-    export interface GetPermittedActionsRequest {
-        WorkflowKey: string;
-        CurrentState: string;
-    }
-
-    export interface GetPermittedActionsResponse {
-        Actions: string[];
-    }
-
-    export namespace WorkflowService {
-        export const baseUrl = 'Services/Workflow';
-
-        export function ExecuteAction(request: ExecuteWorkflowActionRequest) {
-            return Q.post(baseUrl + '/ExecuteAction', request);
-        }
-
-        export function GetPermittedActions(request: GetPermittedActionsRequest) {
-            return Q.post<GetPermittedActionsResponse>(baseUrl + '/GetPermittedActions', request);
-        }
+    export function GetPermittedActions(request: GetPermittedActionsRequest) {
+        return serviceRequest(baseUrl + '/GetPermittedActions', request);
     }
 }
